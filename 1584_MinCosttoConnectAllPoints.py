be3468e65c04675c3972fd68112f0b083e5d45d8
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-01-17 18:44:10 -0300

    add ex medio resolvido com kruskal

---
+++ 1584_MinCosttoConnectAllPoints.py
@@ -0,0 +1,52 @@
+from typing import List
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        def c_distancia(ponto1, ponto2):
+            return abs(ponto1[0] - ponto2[0]) + abs(ponto1[1] - ponto2[1])
+
+        qtdpts = len(points)
+        vertices = []
+
+        for i in range(qtdpts):
+            for j in range(i + 1, qtdpts):
+                distancia = c_distancia(points[i], points[j])
+                vertices.append((distancia, i, j))
+
+        #Ordenar arestas por peso ASC
+        vertices.sort()
+
+        #UF
+        nome_arvore = list(range(qtdpts))
+        rank = [0] * qtdpts
+
+        def find(x):
+            if nome_arvore[x] != x:
+                nome_arvore[x] = find(nome_arvore[x])
+            return nome_arvore[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    nome_arvore[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    nome_arvore[rootX] = rootY
+                else:
+                    nome_arvore[rootY] = rootX
+                    rank[rootX] += 1
+
+        #Construir a Árvore Geradora Mínima (MST)
+        custo = 0
+        usadas = 0 #Num de arestas não descartadas
+
+        for cost, u, v in vertices:
+            if find(u) != find(v):
+                union(u, v)
+                custo += cost
+                usadas += 1
+                if usadas == qtdpts - 1:  #Todos os pontos conectados sem ciclos
+                    break
+
+        return custo
